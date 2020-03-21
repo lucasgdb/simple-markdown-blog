4a99e034c1cd4ffc9a273d3beedf036980d1bc99
Lucas Bittencourt <lucasgdbittencourt@gmail.com>
2020-03-21 09:33:22 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Docker, Mongo and Node with Typescript
+
+## Requirements
+
+- Docker
+- Yarn
+
+## How to
+
+- Clone this repo.
+  - Type `yarn` to install its dependencies.
+  - Type `docker-compose up --build` to build and run the database.
+  - Type `yarn dev` or `yarn build` and `yarn start`.
+
+## Author
+
+| [<img src="https://avatars3.githubusercontent.com/u/13838273?v=3&s=115"><br><sub>@lucasgdb</sub>](https://github.com/lucasgdb) |
+| :----------------------------------------------------------------------------------------------------------------------------: |
+
+#### [Docker + Node + Mongo + React with TypeScript](https://github.com/lucasgdb/docker-mongo-react-node-ts)
+#### [React with TypeScript](https://github.com/lucasgdb/react-ts)
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import { SERVER_PORT } from './configs/default.json';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/api/v1/data', (req: Request, res: Response) =>
+    res.status(200).json({ response: 'hello-world' })
+);
+
+async function main() {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/test', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
+        });
+
+        app.listen(SERVER_PORT, () => console.log('connected.'));
+    } catch (err) {
+        throw err;
+    }
+}
+
+main();
